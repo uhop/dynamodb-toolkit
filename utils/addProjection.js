eb3a796de4d38fed27b5c1bb2d780ea8cd8d5462
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2019-09-13 17:44:18 -0500

    Projection understands subobjects.

--- utils/addProjection.js
+++ utils/addProjection.js
@@ -4,24 +4,39 @@ const fieldsToMap = require('./fieldsToMap');
 
 // form a project expression for DynamoDB
 
-const addProjection = (params, fields, projectionFieldMap, skipSelect) => {
+const isInteger = /^\d+$/;
+
+const addProjection = (params, fields, projectionFieldMap, skipSelect, separator = '.') => {
   if (!fields) return params;
   fields = fieldsToMap(fields, projectionFieldMap);
   if (!fields) return params;
-  const offset = params.ExpressionAttributeNames ? Object.keys(params.ExpressionAttributeNames).length : 0,
-    projectionMap = Object.keys(fields).reduce((acc, name, index) => ((acc['#pr' + (offset + index)] = name), acc), {});
-  if (offset) {
-    Object.assign(params.ExpressionAttributeNames, projectionMap);
-  } else {
-    params.ExpressionAttributeNames = projectionMap;
+  const names = params.ExpressionAttributeNames || {},
+    uniqueNames = {};
+  let keyCounter = Object.keys(names).length;
+  const projection = Object.keys(fields)
+    .reduce((acc, key) => {
+      const path = key.split(separator).map(key => {
+        if (isInteger.test(key)) return key;
+        let alias = uniqueNames['#' + key];
+        if (!alias) {
+          alias = uniqueNames['#' + key] = '#pj' + keyCounter++;
+          names[alias] = key;
+        }
+        return alias;
+      });
+      acc.push(path.reduce((acc, part) => acc + (acc ? (isInteger.test(part) ? '[' + part + ']' : '.' + part) : part), ''));
+      return acc;
+    }, [])
+    .join(', ');
+  if (projection) {
+    Object.keys(names).length && (params.ExpressionAttributeNames = names);
+    if (params.ProjectionExpression) {
+      params.ProjectionExpression += ',' + projection;
+    } else {
+      params.ProjectionExpression = projection;
+    }
+    !skipSelect && params.ProjectionExpression && (params.Select = 'SPECIFIC_ATTRIBUTES');
   }
-  const names = Object.keys(projectionMap).join(',');
-  if (params.ProjectionExpression) {
-    params.ProjectionExpression += ',' + names;
-  } else {
-    params.ProjectionExpression = names;
-  }
-  !skipSelect && params.ProjectionExpression && (params.Select = 'SPECIFIC_ATTRIBUTES');
   return params;
 };
 
