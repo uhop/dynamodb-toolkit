e289f162b4f6d537c3059e661d5c5185385a352c
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2019-10-07 15:26:05 -0500

    Get/apply transaction.

---
+++ utils/applyTransaction.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const combineParams = require('./combineParams');
+
+const doBatch = async (client, batch) => client.transactWriteItems({TransactItems: batch}).promise();
+
+const getTransaction = async (client, ...requests) => {
+  let batch = [];
+
+  for (const request of requests) {
+    if (!request) continue;
+    const params = request.params,
+      base = {};
+    if (params) {
+      params.ConditionExpression && (base.ConditionExpression = params.ConditionExpression);
+      params.ExpressionAttributeNames && (base.ExpressionAttributeNames = params.ExpressionAttributeNames);
+      params.ExpressionAttributeValues && (base.ExpressionAttributeValues = params.ExpressionAttributeValues);
+    }
+    switch (request.action) {
+      case 'check':
+        for (const key of request.keys) {
+          batch.push({ConditionCheck: {TableName: request.table, Key: key, ...base}});
+        }
+        break;
+      case 'delete':
+        for (const key of request.keys) {
+          batch.push({Delete: {TableName: request.table, Key: key, ...base}});
+        }
+        break;
+      case 'put':
+        for (const item of request.items) {
+          batch.push({Delete: {TableName: request.table, Item: item, ...base}});
+        }
+        break;
+      case 'patch':
+        for (const item of request.items) {
+          const p = combineParams(base, item.params),
+            updateItem = {TableName: request.table, Key: item.key, UpdateExpression: p.UpdateExpression};
+          p.ConditionExpression && (updateItem.ConditionExpression = p.ConditionExpression);
+          p.ExpressionAttributeNames && (updateItem.ExpressionAttributeNames = p.ExpressionAttributeNames);
+          p.ExpressionAttributeValues && (updateItem.ExpressionAttributeValues = p.ExpressionAttributeValues);
+          batch.push({Update: updateItem});
+        }
+        break;
+    }
+  }
+  batch.length && await doBatch(client, batch);
+};
+
+module.exports = getTransaction;
---
+++ utils/getTransaction.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const doBatch = async (client, batch) => client.transactGetItems({TransactItems: batch}).promise();
+
+const getTransaction = async (client, ...requests) => {
+  let batch = [];
+
+  for (const request of requests) {
+    if (!request) continue;
+    const params = request.params,
+      base = {};
+    if (params) {
+      params.ProjectionExpression && (base.ProjectionExpression = params.ProjectionExpression);
+      params.ExpressionAttributeNames && (base.ExpressionAttributeNames = params.ExpressionAttributeNames);
+    }
+    switch (request.action) {
+      case 'get':
+        for (const key of request.keys) {
+          batch.push({Get: {TableName: request.table, Key: key, ...base}});
+        }
+        break;
+    }
+  }
+  if (!batch.length) return [];
+  const result = await doBatch(client, batch);
+  return result.responses.map(item => item && item.Item);
+};
+
+module.exports = getTransaction;
