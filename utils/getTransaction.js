49258bbc37b0d06c136fe56b05a7b1e4cd5db9bc
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2019-10-08 10:36:50 -0500

    Added adapters.

--- utils/getTransaction.js
+++ utils/getTransaction.js
@@ -3,7 +3,7 @@
 const doBatch = async (client, batch) => client.transactGetItems({TransactItems: batch}).promise();
 
 const getTransaction = async (client, ...requests) => {
-  let batch = [];
+  let batch = [], adapters = [];
 
   for (const request of requests) {
     if (!request) continue;
@@ -17,13 +17,14 @@ const getTransaction = async (client, ...requests) => {
       case 'get':
         for (const key of request.keys) {
           batch.push({Get: {TableName: request.table, Key: key, ...base}});
+          adapters.push(request.adapter);
         }
         break;
     }
   }
   if (!batch.length) return [];
   const result = await doBatch(client, batch);
-  return result.responses.map((item, index) => ({table: batch[index].TableName, item: item && item.Item}));
+  return result.responses.map((item, index) => ({table: batch[index].TableName, item: item && item.Item, adapter: adapters[index]}));
 };
 
 module.exports = getTransaction;
