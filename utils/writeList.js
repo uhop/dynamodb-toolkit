82ca777976f5399559adcf08779670981d973519
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2019-11-07 11:46:06 -0600

    Converted writeList() to applyBatch().

--- utils/writeList.js
+++ utils/writeList.js
@@ -2,25 +2,16 @@
 
 // iteratively copy a list of items by keys
 
-const batchWrite = require('./batchWrite');
-
-const writeKeyList = async (client, tableName, items) => {
-  items = items.filter(item => item);
-  if (!items.length) return;
-  const params = {
-    RequestItems: {
-      [tableName]: items.map(item => ({PutRequest: {Item: item}}))
-    }
-  };
-  return batchWrite(client, params);
-};
+const applyBatch = require('./applyBatch');
 
 const writeList = async (client, tableName, items, mapFn) => {
-  // iterate over parameters copying records
-  for (let i = 0; i < items.length; i += 25) {
-    // select items of 25
-    await writeKeyList(client, tableName, items.slice(i, i + 25).map(mapFn));
-  }
+  return applyBatch(
+    client,
+    items
+      .map(mapFn)
+      .filter(item => item)
+      .map(item => ({action: 'put', params: {TableName: tableName, Item: item}}))
+  );
 };
 
 module.exports = writeList;
