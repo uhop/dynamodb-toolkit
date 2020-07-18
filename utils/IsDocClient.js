37fd507b1faff501828bf9c588241f3368329042
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2020-07-18 15:14:00 -0500

    Removed unused file.

--- utils/IsDocClient.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const AWS = require('aws-sdk');
-
-const DocumentClient = AWS.DynamoDB.DocumentClient;
-
-const isDocClient = client => client instanceof DocumentClient;
-
-module.exports = isDocClient;
