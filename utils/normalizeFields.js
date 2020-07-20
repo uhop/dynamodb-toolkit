fba274256e7a9c7e0d493e89e5e34eb3accb26af
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2020-07-19 20:48:51 -0500

    Added field normalization.
    It supports complex names.

---
+++ utils/normalizeFields.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const normalizeFields = (fields, projectionFieldMap, separator = '.') => {
+  if (!fields) return null;
+  if (!Array.isArray(fields)) {
+    if (typeof fields == 'string') {
+      fields = fields
+        .split(',')
+        .trim()
+        .filter(f => f);
+    } else if (typeof fields == 'object') {
+      fields = Object.keys(fields);
+    } else {
+      return null;
+    }
+  }
+  if (!projectionFieldMap) return fields;
+  return fields.map(name => {
+    const parts = name.split(separator),
+      replacement = projectionFieldMap[parts[0]];
+    if (typeof replacement == 'string') {
+      parts[0] = replacement;
+      return parts.join(separator);
+    }
+    return name;
+  });
+};
+
+module.exports = normalizeFields;
