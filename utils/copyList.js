96baad3be5f057a0400be01a8770c19a9f4ebeae
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2020-07-27 22:22:03 -0500

    Improved copyList().

--- utils/copyList.js
+++ utils/copyList.js
@@ -1,31 +1,35 @@
 'use strict';
 
-// iteratively copy a list of items by keys
+// iteratively copy a list of items by params
 
 const readList = require('./readList');
 const writeList = require('./writeList');
 
 const copyListByKeys = require('./copyListByKeys');
 
-const copyList = async (client, params, mapFn) => {
+const identity = x => x;
+
+const copyList = async (client, params) => {
+  const tableName = params.TableName;
   let processed = 0;
   while(params) {
     params = await readList(client, params, async data => {
-      if (data.Items.length) {
-        processed += await writeList(client, params.TableName, data.Items, mapFn);
+      const items = data.Items;
+      for (let offset = 0; offset < items.length; offset += 25) {
+        const slice = items.slice(offset, offset + 25);
+        processed += await writeList(client, tableName, slice, mapFn);
       }
     });
   }
   return processed;
 };
 
-copyList.viaKeys = async (client, params, mapFn) => {
-  const tableName = params.TableName;
+copyList.viaKeys = async (client, params, mapFn, keyFn = identity) => {
   let keys = [];
   while(params) {
-    params = await readList(client, params, async data => (keys = keys.concat(data.Items)));
+    params = await readList(client, params, async data => (keys = keys.concat(data.Items.map(keyFn).filter(identity))));
   }
-  return copyList.byKeys(client, tableName, keys, mapFn);
+  return copyList.byKeys(client, params.TableName, keys, mapFn);
 };
 
 copyList.byKeys = copyListByKeys;
