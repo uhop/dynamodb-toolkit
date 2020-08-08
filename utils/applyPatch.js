9ba591ef3de510fd51e3c4d5e3a6a9191508f34b
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2020-08-08 16:07:24 -0500

    Added a separator argument.

--- utils/applyPatch.js
+++ utils/applyPatch.js
@@ -3,12 +3,12 @@
 const setPath = require('./setPath');
 const deletePath = require('./deletePath');
 
-const applyPatch = (o, patch) => {
+const applyPatch = (o, patch, separator = '.') => {
   Object.keys(patch).forEach(path => {
     if (path == '__delete') {
-      patch.__delete.forEach(path => deletePath(o, path));
+      patch.__delete.forEach(path => deletePath(o, path, separator));
     } else {
-      setPath(o, path, patch[path]);
+      setPath(o, path, patch[path], separator);
     }
   });
   return o;
