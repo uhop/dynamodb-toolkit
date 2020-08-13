538c1bdc09fcee1fd71bbdf23be8d27ffb654737
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2020-08-13 15:26:32 -0500

    A different way to override params.

--- utils/readListByKeys.js
+++ utils/readListByKeys.js
@@ -7,7 +7,10 @@
 const getBatch = require('./getBatch');
 
 const readListByKeys = async (client, tableName, keys, params) => {
-  const result = await getBatch(client, keys.map(key => ({action: 'get', params: Object.assign({TableName: tableName, Key: key}, params)})));
+  const result = await getBatch(
+    client,
+    keys.map(key => ({action: 'get', params: Object.assign({}, params, {TableName: tableName, Key: key})}))
+  );
   return result.map(pair => pair.item);
 };
 
